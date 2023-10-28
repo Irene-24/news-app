095c8d1cfb4f871e4262a96392f4b192deb5b01f
Irene-24 <okaforirenen@gmail.com>
2023-10-28 09:54:34 +0100

    added a basic test for <SideBar/>

--- __tests__/components/SideBar.cy.tsx
+++ __tests__/components/SideBar.cy.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import SideBar from "@/components/navigation/SideBar";
 
 describe("<SideBar />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
+  it("renders correctly", () => {
     cy.mount(<SideBar />);
+
+    cy.contains(".first a", /top stories/i);
+
+    cy.get('[alt="News app logo"]').should("be.visible");
+
+    cy.get("li a").should("have.length", 5);
   });
 });
